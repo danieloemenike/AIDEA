0a23783f606ec5c1f41072abebb77f2a4d67a191
Daniel Emenike <danieloemenike@gmail.com>
2023-11-07 18:15:27 +0000

    pinecone correction made

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeClient } from "@pinecone-database/pinecone";
+import { Pinecone } from "@pinecone-database/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 export type AideaKey = {
@@ -12,19 +12,19 @@ export type AideaKey = {
 export class MemoryManager {
   private static instance: MemoryManager;
   private history: Redis;
-  private vectorDBClient: PineconeClient;
+  private vectorDBClient: Pinecone;
 
   public constructor() {
     this.history = Redis.fromEnv();
-    this.vectorDBClient = new PineconeClient();
+    this.vectorDBClient = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY!,
+      environment: process.env.PINECONE_ENVIRONMENT!,
+    });
   }
 
   public async init() {
-    if (this.vectorDBClient instanceof PineconeClient) {
-      await this.vectorDBClient.init({
-        apiKey: process.env.PINECONE_API_KEY!,
-        environment: process.env.PINECONE_ENVIRONMENT!,
-      });
+    if (this.vectorDBClient instanceof Pinecone) {
+      await this.vectorDBClient;
     }
   }
 
@@ -32,7 +32,7 @@ export class MemoryManager {
     recentChatHistory: string,
     companionFileName: string
   ) {
-    const pineconeClient = <PineconeClient>this.vectorDBClient;
+    const pineconeClient = <Pinecone>this.vectorDBClient;
 
     const pineconeIndex = pineconeClient.Index(
       process.env.PINECONE_INDEX! || ""
